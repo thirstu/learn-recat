47854d5036061e2a05e81a09ddedf5f3549ed22c
thirstu <ajayknpc185@gmail.com>
2023-10-05 00:05:32 +0530

    react basics

---
+++ learnReact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+   <h1>Hello Mr. /Ajay</h1>
+  )
+}
+
+export default App
---
+++ reactbasics/src/App.js
@@ -0,0 +1,9 @@
+
+function App() {
+  return (
+   <h1>hello basics
+   </h1>
+  );
+}
+
+export default App;
