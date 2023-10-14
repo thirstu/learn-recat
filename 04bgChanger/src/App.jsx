087a8e182030d850854b3a8c1916dd0fc1a4211f
thirstu <ajayknpc185@gmail.com>
2023-10-14 23:27:26 +0530

    04prject complete

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -10,34 +10,53 @@ function App() {
   const button=document.querySelector('button');
   // mainCOntainer.style.backgroundColor='#FFFFFF';
   // body.style.backgroundColor='#FFFFFF';
-  console.log(mainCOntainer);
+  // console.log(mainCOntainer);
 
   const changeColor=function(color){
   body.style.backgroundColor=color;
-  const [a,b]=useState()
-  console.log(event.target);
-  }
+  
+  };
+///////////////
+// const btnBgColor=()=>{
+//   const btns=mainCOntainer?.querySelectorAll('.btn');
+//   console.log(...btns,mainCOntainer.querySelectorAll('.btn'));
+  
+//   [...btns].map(btn=>{
+//     const value=btn.textContent.toLowerCase();
+//     btn.style.backgroundColor=value;
+//     console.log('hello');
 
-//   useEffect(()=>{
-//     const buttons=document.querySelectorAll('button');
-// console.log(button);
+//   });
+// };
+// btnBgColor();
+(()=>{
+  const btns=mainCOntainer.querySelectorAll('.btn');
+  console.log(...btns,mainCOntainer.querySelectorAll('.btn'));
+  
+  [...btns].map(btn=>{
+    const value=btn.textContent.toLowerCase();
+    btn.style.backgroundColor=value;
+    console.log('hello');
 
-//   },[]);
+  });
+}
+)();
+// console.log(mainCOntainer.querySelectorAll('.btn')[1].textContent.toLowerCase());
+// mainCOntainer.querySelectorAll('.btn')[1].style.textTransform = "capitalize";
 
-console.log(button);
   return (
     <>
       
-      <div className='flex border-4 p-3 rounded-xl relative  bottom--20  justify-center'>
-        <button className='mr-3 font-bold bg-gray-500' onClick={()=>changeColor('red')}>RED</button>
-        <button className='mr-3 font-bold bg-gray-500' onClick={()=>changeColor('yellow')}>YELLOW</button>
-        <button className='mr-3 font-bold bg-gray-500' onClick={()=>changeColor('pink')}>PINK</button>
-        <button className='mr-3 font-bold bg-gray-500' onClick={()=>changeColor('green')}>GREEN</button>
-        <button className='mr-3 font-bold bg-gray-500' onClick={()=>changeColor('gray')}>GRAY</button>
-        <button className='mr-3 font-bold bg-gray-500' onClick={()=>changeColor('orange')}>ORANGE</button>
-        <button className='mr-3 font-bold bg-gray-500' onClick={()=>changeColor('blue')}>BLUE</button>
-        <button className='mr-3 font-bold bg-gray-500' onClick={()=>changeColor('brown')}>BROWN</button>
-        <button className='mr-3 font-bold bg-gray-500' onClick={()=>changeColor('white')}>WHITE</button>
+      <div className='btnsContainer flex border-4 p-3 rounded-xl relative  bottom--20  justify-center'>
+        <button className='btn mr-3 font-bold bg-gray-500' onClick={(e)=>changeColor('red')}>RED</button>
+        <button className='btn mr-3 font-bold bg-gray-500' onClick={()=>{changeColor('yellow')}}>YELLOW</button>
+        <button className='btn mr-3 font-bold bg-gray-500' onClick={()=>changeColor('pink')}>PINK</button>
+        <button className='btn mr-3 font-bold bg-gray-500' onClick={()=>changeColor('green')}>GREEN</button>
+        <button className='btn mr-3 font-bold bg-gray-500' onClick={()=>changeColor('gray')}>GRAY</button>
+        <button className='btn mr-3 font-bold bg-gray-500' onClick={()=>changeColor('orange')}>ORANGE</button>
+        <button className='btn mr-3 font-bold bg-gray-500' onClick={()=>changeColor('blue')}>BLUE</button>
+        <button className='btn mr-3 font-bold bg-gray-500' onClick={()=>changeColor('brown')}>BROWN</button>
+        <button className='btn mr-3 font-bold bg-gray-500' onClick={()=>changeColor('white')}>WHITE</button>
       </div>
     
       
